906071c573519c919577be071935e0574a10b842
Jarrad Henry - Vendor <jhenry@m-C02RJ1BUG8WM.local>
2016-10-05 14:22:57 -0700

    update

--- src/App.js
+++ src/App.js
@@ -11,45 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-
-    function prodCell(params) {
-        return '<span class="tag-item">' + params.value.toUpperCase() + '</span>';
-    }
-
-    function linkCell(params) {
-        return '<a class="name-link" href="#">' + params.value.toUpperCase() + '</a>';
-    }
-
-    function tagName(params) {
-        return '<span class="tag-item">' + params.value.tagName.toUpperCase() + '</span>';
-    }
-
     var component = this;
-
-    this.state = {
-      currentView : 'app',
-      columnDefs : [
-        {headerName: "Name", field: "name", width:500, cellRenderer: linkCell},
-        {headerName: "Environment Type", field: "environmentTypes", width: 300, cellClass: "type-cell", cellRenderer: prodCell},
-        {headerName: "Tags", field: "tags", width: 350, cellClass: "tag-name", cellRenderer: tagName}],
-      rowData: null,
-        enableSorting: true,
-        enableFilter: true,
-        rowHeight: 40
-
-    };
-
-    $.when($.getJSON( "/registry/rs/ga/v2/ui/search/application"),
-      $.getJSON( "/registry/rs/ga/v2/ui/search/application")).then(function( result1, result2 ) {
-        var mergedResults = result1[0].searchResult.hits.concat(result2[0].searchResult.hits);
-        component.setState({rowData: mergedResults})
-
-        console.log('*******Merged Results********')
-        console.dir(mergedResults)
-    });
-
-
-
   }
 
   render() {
